27b8e201d136a46994f3d48d7226d46bc0be4dc3
mahdichinnawi1-sketch <mahdi.chinnawi1@gmail.com>
2026-05-14 06:23:54 -0700

    Add files via upload

---
+++ flutter_application_1/lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static const List<String> days = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+  ];
+  
+  static const List<String> workoutTypes = [
+    '🔥 Weight Loss', '⚡ Endurance', '💪 Muscle Building'
+  ];
+  
+  static const List<String> locations = ['🏠 Home', '🏋️ Gym'];
+}
